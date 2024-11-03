9643e45174b70ab01abc00e2a7c9d6dcbe66b61e
clementdevarieux <devarieux.clement@gmail.com>
2024-11-03 19:22:51 +0100

    new models

--- src/modeling/train_model.py
+++ src/modeling/train_model.py
@@ -2,22 +2,26 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
 import joblib
 from nltk.corpus import stopwords
 from nltk.stem.snowball import FrenchStemmer
 import nltk
+import random
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+
 
 nltk.download('stopwords')
 
-def train(input_filename, model_dump_filename):
-    # stemmer = FrenchStemmer()
-    # analyzer = CountVectorizer().build_analyzer()
-    #
-    # def stemmed_words(doc):
-    #     return (stemmer.stem(w) for w in analyzer(doc))
+def stemmed_words(doc):
+    stemmer = FrenchStemmer()
+    analyzer = CountVectorizer().build_analyzer()
+    return (stemmer.stem(w) for w in analyzer(doc))
 
+def train(input_filename, model_dump_filename):
     data = pd.read_csv(input_filename)
     X = data['video_name']
     y = data['is_comic']
@@ -25,14 +29,9 @@ def train(input_filename, model_dump_filename):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     pipeline = Pipeline([
-        ('vectorizer', CountVectorizer(stop_words=stopwords.words('french'), lowercase=False)),
+        ('vectorizer', CountVectorizer(analyzer='char', ngram_range=(1, 6))),
         ('classifier', RandomForestClassifier())
     ])
-
-    # pipeline = Pipeline([
-    #     ('vectorizer', CountVectorizer(analyzer=stemmed_words)),
-    #     ('classifier', RandomForestClassifier())
-    # ]) ## dump not working
 
     pipeline.fit(X_train, y_train)
 
